518f2d4590dbcc09f70e996550631b35e11390f1
anukulghoshdev <anukulghosh264@gmail.com>
2022-11-10 22:22:32 +0600

    verifyJWT added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json()) // Middleware for convert into json
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('ass-10 server is running...');
 })
 //-----------------------
@@ -26,33 +26,38 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-function verfiyJWT(req, res, next){
-    const authHeader = req.headers.authorization; 
-    
-
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized access'});  
+function verfiyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
     }
-    const token = authHeader.split(' ')[1];      // ['Bearer', 'eyJhbGciOiJI...']
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-        if(err){
-            return res.status(403).send({message: 'Forbidden'});
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden' });
         }
-
-        req.decoded = decoded;  // { email: 'anukulghosh4@gmail.com', iat: 1667742143, exp: 1667745743 }
-        next()
+        req.decoded = decoded;
+        next();
     })
-    
+
 }
 
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         const serviceCollection = client.db('ass10').collection('services');
         const reviewCollection = client.db('ass10').collection('reviews');
 
-        app.post('/services', async(req, res)=>{
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+
+
+        app.post('/services', async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
             // console.log(result);
@@ -74,9 +79,9 @@ async function run(){
             res.send(services);
         });
 
-        app.get('/services/:id', async(req, res)=>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             // console.log(service);
             res.send(service)
@@ -91,13 +96,13 @@ async function run(){
         })
 
         // get all reviews
-        
+
 
         // get reviews of specific services
-        app.get('/reviews', async(req,res)=>{
+        app.get('/reviews', async (req, res) => {
             let query = {}
-            if (req.query.service_id){
-                query={
+            if (req.query.service_id) {
+                query = {
                     service_id: req.query.service_id
                 }
             }
@@ -107,10 +112,15 @@ async function run(){
         })
 
         //get reviews for specifit user/user email
-        app.get('/myreviews', async(req,res)=>{
+        app.get('/myreviews', verfiyJWT, async (req, res) => {
+
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
             let query = {}
-            if (req.query.email){
-                query={
+            if (req.query.email) {
+                query = {
                     user_Email: req.query.email
                 }
             }
@@ -120,9 +130,9 @@ async function run(){
         })
 
         // delete a review
-        app.delete('/myreviews/:id', async(req,res)=>{
+        app.delete('/myreviews/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await reviewCollection.deleteOne(query);
             // console.log(result);
             res.send(result);
@@ -132,18 +142,18 @@ async function run(){
 
 
         // get single review then update
-        app.get('/myreviews/:id', async(req, res)=>{
+        app.get('/myreviews/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await reviewCollection.findOne(query);
             // console.log(result);
             res.send(result);
         })
 
-        app.put('/myreviews/:id', async(req,res)=>{
+        app.put('/myreviews/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
-            console.log(query , id);
+            const query = { _id: ObjectId(id) };
+            console.log(query, id);
             const review = req.body;
             console.log(review.reviewPost);
             // const option = {upsert:true} ;
@@ -159,15 +169,15 @@ async function run(){
 
         })
 
-        
-    }
-
-    finally{
 
     }
-    
+
+    finally {
+
+    }
+
 }
-run().catch(e=>console.log(e.message))
+run().catch(e => console.log(e.message))
 
 
 
@@ -177,6 +187,6 @@ run().catch(e=>console.log(e.message))
 
 
 //----------------------
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('ass-10 server running on port', port);
 })
